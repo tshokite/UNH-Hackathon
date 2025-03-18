8ba63fc73bf9231bcf2cf568b1165fbcfa6eb537
Thomas Shokite <tshokite@newhaven.edu>
2025-03-18 12:51:18 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from tkinter import *
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    root = Tk()
+    myLabel = Label(root, text="Hello CSCI 6651 Python Class!")
+    myLabel.pack()
+    root.mainloop()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
